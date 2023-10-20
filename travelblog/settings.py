95dc1c0e9454c733239d5e8c49cae2816145acb1
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-20 20:13:36 +0200

    fix: amend Procfile

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "project-4-gennadiy-gaysha-3d98b941946a.herokuapp.com"]
 
 
 # Application definition
