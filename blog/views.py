986e06a31155ac03fb1e6613230a1bcd3e737e31
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-22 15:41:43 +0200

    feat: create PostList view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """
+    A view that displays a list of published blog posts on the home page.
+
+    Attributes:
+    model: The model used for retrieving data, in this case, the Post model.
+    template: The template used for rendering the view, here, the 'blog/home.html' template.
+    queryset: The queryset used to retrieve all published posts, filtering the posts based
+    on their status (status=2) and ordering them by their creation date in descending order.
+    paginate_by: The number of posts displayed per page, set to 3 in this case.
+    """
+    model=Post
+    template = 'blog/home.html'
+    queryset = Post.objects.filter(status=2).order_by('-created_on')
+    paginate_by = 3
