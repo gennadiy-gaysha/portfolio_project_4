3417a6b78ff6c7b6243b9f47e42b6202c9dd68d4
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-22 14:26:43 +0200

    feat: add Country, Post and Comment to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,45 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Country, Post, Comment
 
 
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ['country_name']
 
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
+    list_display = ['title', 'status', 'created_on']
+    list_filter = ['status', 'created_on']
+    search_fields = ['title', 'content']
+    ordering = ['-created_on']
+    summernote_fields = ['content']
+    actions = ['approve_posts']
+
+    def approve_posts(self, queryset):
+        """
+        Updates the status of the selected posts in the queryset
+        to a value of 2, indicating that they have been approved.
+        """
+        queryset.update(status=2)
+
+    approve_posts.short_description = 'Approve posts'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'body', 'post', 'created_on', 'approved']
+    list_filter = ['approved', 'created_on']
+    search_fields = ['name', 'email', 'address', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, queryset):
+        """
+        Updates the status of the selected comments in the queryset
+        to a value of True, indicating that they have been approved.
+        """
+        queryset.update(approved=True)
+
+    approve_comments.short_description = 'Approve comments'
