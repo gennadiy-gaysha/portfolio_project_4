6fa62c1d4541f527e7b58e42dfc79f070dc2e7b1
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-22 18:07:58 +0200

    feat: create starter base and home templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
@@ -9,12 +8,12 @@ class PostList(generic.ListView):
 
     Attributes:
     model: The model used for retrieving data, in this case, the Post model.
-    template: The template used for rendering the view, here, the 'blog/home.html' template.
+    template_name: The template used for rendering the view, here, the 'blog/home.html' template.
     queryset: The queryset used to retrieve all published posts, filtering the posts based
     on their status (status=2) and ordering them by their creation date in descending order.
     paginate_by: The number of posts displayed per page, set to 3 in this case.
     """
-    model=Post
+    model = Post
     template_name = 'blog/home.html'
     queryset = Post.objects.filter(status=2).order_by('-created_on')
     paginate_by = 3
