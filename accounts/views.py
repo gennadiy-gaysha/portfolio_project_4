ef641a3ec51049cc0a1edd541c743d50b826c915
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-23 12:33:31 +0200

    feat: create view for registering a user account

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+class RegisterNewAccount(CreateView):
+    """
+    A view for registering a new user account.
+
+    Attributes:
+    form_class: The form class used for creating a new user account. In this case,
+    it is the UserCreationForm.
+    template_name: The template used for rendering the registration form, here,
+    'registration/register.html'.
+    success_url: The URL to redirect to upon successful registration, specified
+    using the reverse_lazy function and the 'login' URL name.
+    """
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url =  reverse_lazy('login')
--- travelblog/settings.py
+++ travelblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django_summernote',
 
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
