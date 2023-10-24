76239b1d8486738806a541999b59e802c6d27445
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-24 22:34:01 +0200

    feat: create path to trigger UpdatePost view

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetails.as_view(), name='post-details'),
-    path('create_post/', views.CreatePost.as_view(), name='create-post')
+    path('create_post/', views.CreatePost.as_view(), name='create-post'),
+    path('post/update/<slug:slug>', views.UpdatePost.as_view(), name='update-post'),
 ]
