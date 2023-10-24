af9e9db2edecf8a3702b48ab40ed6676af60318b
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-24 12:38:20 +0200

    feat: create CreatePost view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from django.views.generic import CreateView
+
 from .models import Post
 
 
@@ -29,3 +32,36 @@ class PostDetails(generic.DetailView):
     """
     model = Post
     template_name = 'blog/post_details.html'
+
+class CreatePost(LoginRequiredMixin, CreateView):
+    """
+    A class-based view that enables the creation of a new blog post.
+
+    Attributes:
+    model: The model to be used for creating the post, in this case, the Post
+    model.
+    template_name: The template used for rendering the create post view,
+    'blog/create_post.html'.
+    fields: The fields from the model that are used for the form. These
+    fields will be displayed in the template for the user to fill out.
+    """
+    model = Post
+    template_name = 'blog/create_post.html'
+    fields = ('title', 'author', 'content', 'featured_image')
+
+    def form_valid(self, form):
+        """
+        This method is being overriden in CreatePost view to provide custom
+        logic that should be executed when the submitted form data is valid
+        Automatically adds current logged-in user as the author of the post.
+
+        Args:
+        form: The form that is to be validated.
+
+        Returns:
+        super() is used to call the parent class's method. It calls the
+        form_valid method of the parent class (CreateView) to ensure that
+        the standard behavior of the CreateView is executed.
+        """
+        form.instance.user = self.request.user
+        return super().form_valid(form)
