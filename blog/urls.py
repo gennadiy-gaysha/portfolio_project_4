68c30ced76478e206826f7f4d0a67e4c70ca7ae6
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-24 12:43:36 +0200

    feat: create path to trigger CreatePost view

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetails.as_view(), name='post-details'),
+    path('create_post/', views.CreatePost.as_view(), name='create-post')
 ]
