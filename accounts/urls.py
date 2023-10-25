f543fcd7ac804fe7390e28a03edf6c123eaccd18
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-25 21:00:37 +0200

    feat: create user EditDetails view and path to it

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from accounts import views
 
 urlpatterns = [
     path('register/', views.RegisterNewAccount.as_view(), name='register'),
+    path('edit_details/', views.EditDetails.as_view(), name='edit-details')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views import generic
 
 from accounts.forms import RegisterForm
 
 
-class RegisterNewAccount(CreateView):
+class RegisterNewAccount(generic.CreateView):
     """
     A view for registering a new user account.
 
@@ -19,4 +19,30 @@ class RegisterNewAccount(CreateView):
     """
     form_class = RegisterForm
     template_name = 'registration/register.html'
-    success_url =  reverse_lazy('login')
+    success_url = reverse_lazy('login')
+
+
+class EditDetails(generic.UpdateView):
+    """
+    A view for editing user registration details.
+
+    Attributes:
+    form_class: The form class used for updating user details, which is set to
+    UserChangeForm.
+    template_name: The name of the template used for rendering the user details edit
+    page, set to 'registration/edit_details.html'.
+    success_url: The URL to redirect to upon successfully updating the user details,
+    utilizing the reverse_lazy method.
+    """
+    form_class = UserChangeForm
+    template_name = 'registration/edit_details.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        """
+        Retrieves the current logged-in user.
+
+        Returns:
+        The user object representing the currently logged-in user.
+        """
+        return self.request.user
