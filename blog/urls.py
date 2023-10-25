e0f597b3d89ab4da973a51113f19ee5cace6b258
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-25 14:48:40 +0200

    fix: amend path for UpdatePost view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetails.as_view(), name='post-details'),
     path('create_post/', views.CreatePost.as_view(), name='create-post'),
-    path('post/update/<slug:slug>/', views.UpdatePost.as_view(), name='update-post'),
+    path('update_post/<slug:slug>/', views.UpdatePost.as_view(), name='update-post'),
     path('delete_post/<slug:slug>', views.DeletePost.as_view(), name='delete post'),
     path('draft_list/<str:username>/', views.DraftList.as_view(), name='draft-list'),
     path('published_list/<str:username>/', views.PublishedList.as_view(),
