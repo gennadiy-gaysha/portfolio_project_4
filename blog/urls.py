c1ce37db9953c1e8a8e32478fc546b052f5d7e4c
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-25 19:14:17 +0200

    feat: add Delete and Reject button to post's pages

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('post/<slug:slug>/', views.PostDetails.as_view(), name='post-details'),
     path('create_post/', views.CreatePost.as_view(), name='create-post'),
     path('update_post/<slug:slug>/', views.UpdatePost.as_view(), name='update-post'),
-    path('delete_post/<slug:slug>/', views.DeletePost.as_view(), name='delete post'),
+    path('delete_post/<slug:slug>/', views.DeletePost.as_view(), name='delete-post'),
     path('draft_list/<str:username>/', views.DraftList.as_view(), name='draft-list'),
     path('published_list/<str:username>/', views.PublishedList.as_view(),
          name='published-list'),
