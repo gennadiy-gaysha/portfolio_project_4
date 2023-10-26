f4848a4082d364592bff97b350a36338e358616f
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-26 13:34:21 +0200

    create path and template to ChangePassword view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from accounts import views
 
 urlpatterns = [
     path('register/', views.RegisterNewAccount.as_view(), name='register'),
-    path('edit_details/', views.EditDetails.as_view(), name='edit-details')
+    path('edit_details/', views.EditDetails.as_view(), name='edit-details'),
+    path('change_password/', views.ChangePassword.as_view(), name='change-password'),
 ]
