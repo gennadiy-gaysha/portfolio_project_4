80706e8d3bf99ecdc4aa963c881e4b096946eee1
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-26 13:25:28 +0200

    feat: create ChangePasswordForm for ChangePassword

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 
 
@@ -84,3 +84,16 @@ class EditDetailsForm(UserChangeForm):
         self.fields[
             'username'].help_text = '<span style="color: green">Disclaimer: Once ' \
             'created, you cannot change your username.</span>'
+
+
+class ChangePasswordForm(PasswordChangeForm):
+    class Meta:
+        model = User
+        fields = ['old_password', 'new_password1', 'new_password2']
+
+    def __init__(self, *args, **qwargs):
+        super().__init__(*args, **qwargs)
+        self.fields['old_password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['new_password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['new_password2'].widget.attrs.update({'class': 'form-control'})
+
