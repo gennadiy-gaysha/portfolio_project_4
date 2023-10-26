dc63b381744dfc0cadd1a5b5219fdbad3cbc7de5
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-26 15:42:23 +0200

    feat: create UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField ('image', default='placeholder')
+    bio = models.TextField(blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'),
+                                       ('Other', 'Other')], blank=True)
+    home_country = models.CharField(max_length=100, blank=True)
+    instagram_profile = models.URLField(blank=True)
+    twitter_profile = models.URLField(blank=True)
+    facebook_profile = models.URLField(blank=True)
+    linkedin_profile = models.URLField(blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+
