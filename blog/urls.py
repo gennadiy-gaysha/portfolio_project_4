1422dd531dc90cfca135ccf0709590f2d72d850b
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-28 21:16:18 +0200

    feat: create path and temlate for search_country view

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('create_post/', views.CreatePost.as_view(), name='create-post'),
     path('update_post/<slug:slug>/', views.UpdatePost.as_view(), name='update-post'),
     path('delete_post/<slug:slug>/', views.DeletePost.as_view(), name='delete-post'),
+    path('search_country/', views.search_country, name='search-country'),
     path('draft_list/<str:username>/', views.DraftList.as_view(), name='draft-list'),
     path('published_list/<str:username>/', views.PublishedList.as_view(),
          name='published-list'),
