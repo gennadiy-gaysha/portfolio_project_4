8da6cff6b78b7fa7721566f4d77ee24b62073974
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-29 16:16:19 +0100

    feat: create PostFilter to filter posts by country

---
+++ blog/filters.py
@@ -0,0 +1,25 @@
+import django_filters
+from django import forms
+from .models import Post, Country
+
+
+class PostFilter(django_filters.FilterSet):
+    """
+    Filter class for filtering Post objects based on the selected country.
+
+    Attributes:
+        country: django_filters.ModelChoiceFilter
+            A filter for selecting the country from a list of all countries.
+            The queryset is ordered by country name.
+            The filter is represented as a dropdown select form element.
+    """
+    country = django_filters.ModelChoiceFilter(
+        queryset=Country.objects.all().order_by('country_name'),
+        widget=forms.Select(attrs={'class': 'form-control', 'style': 'width: 20%; display: inline;',
+                                   'label': 'Filter posts by country: '}),
+        label='Filter by country',
+    )
+
+    class Meta:
+        model = Post
+        fields = ['country']
