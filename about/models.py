78a0e14f77ab5ca9cbbc86369091c1097907b778
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-29 19:43:41 +0100

    feat: create about_page.html

--- about/models.py
+++ about/models.py
@@ -3,7 +3,7 @@ from cloudinary.models import CloudinaryField
 
 
 class About(models.Model):
-    title = models.CharField(max_length=200, blank=True)
+    title    = models.CharField(max_length=200, blank=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
 
