442f9aa33fc6a7ddd8e07a7cf2971754fa102f40
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-31 15:22:57 +0100

    feat: add fath for PostLike view

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('update_post/<slug:slug>/', views.UpdatePost.as_view(), name='update-post'),
     path('delete_post/<slug:slug>/', views.DeletePost.as_view(), name='delete-post'),
     path('search_country/', views.search_country, name='search-country'),
+    path('like/<slug:slug>/', views.PostLike.as_view(), name='post-like'),
     path('<slug:slug>/', views.ShowCountry.as_view(), name='show-country'),
     path('draft_list/<str:username>/', views.DraftList.as_view(), name='draft-list'),
     path('published_list/<str:username>/', views.PublishedList.as_view(),
--- blog/views.py
+++ blog/views.py
@@ -104,15 +104,15 @@ class PostDetails(View):
                        'liked': liked, 'comment_form': CommentForm(), 'commented': True}, )
 
 
-class PostLikes(View):
-    def post(self, request, slug):
+class PostLike(View):
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
 
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        return HttpResponseRedirect(reverse('post-details', args=[slug]))
 
 class CreatePost(LoginRequiredMixin, CreateView):
     """
