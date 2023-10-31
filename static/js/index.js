a5a43353f2f7b4e147b74d51408418daace5602f
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-10-31 20:24:17 +0100

    feat: create script for script for auto-dismissing flash messages

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function(){
+        let messages = document.getElementById('msg');
+        let alert = new bootstrap.Alert(messages);
+        alert.close();
+    }, 2000);
+});
+
+
+
+
+
+
