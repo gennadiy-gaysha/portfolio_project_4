15f1eea0e4c967d12bd3f0b64c2d917ebd59ecaf
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2023-11-03 16:58:51 +0100

    feat: add script to clear the comment form after submition

---
+++ blog/static/blog/js/comment.js
@@ -0,0 +1,35 @@
+// function to clear the comment form
+function clear_form() {
+    document.getElementById('comment_form').reset();
+}
+
+// add event listener to the form submission
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('comment_form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        var form = event.target;
+        var formData = newFormData(form);
+
+        // Perform an AJAX request to submit the form data
+        fetch(form.action, {
+            method: form.method,
+            body: formData
+            })
+            .then(function(response) {
+            // Handle the response as needed
+            // Display the success message
+            var message = document.createElement('div');
+            message.classList.add('alert', 'alert-success', 'mt-3');
+            message.textContent = 'Your comment is awaiting approval';
+            message.style.width = '100%';
+
+            var formElement = document.getElementById("comment_form");
+            formElement.parentNode.insertBefore(message, formElement.nextSibling);
+            // Remove the message after 5 seconds
+            setTimeout(function () {
+                    message.remove();
+                }, 5000);
+                clearForm(); // Clear the form fields
+            });
+        });
+    });
